42d4629c2c74ea0dcc60522b74d3865c79f8ec54
Page <3067822417@qq.com>
2026-02-13 00:19:07 +0800

    主页面大致完成

--- Assets/Script/MainMenu/CollectionSlot.cs
+++ Assets/Script/MainMenu/CollectionSlot.cs
@@ -1,21 +1,43 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class CollectionSlot : MonoBehaviour
 {
     public string collectibleID;
     public Image itemImage;
+    [TextArea] public string description;
+
+    public TMP_Text descriptionTextUI;
+    public GameObject descriptionPanel;
+
+    [HideInInspector]
+    public bool isUnlocked;   //由 CollectionPage 控制
 
     public void SetDisplay(bool unlocked)
     {
+        isUnlocked = unlocked;   //记录状态
         itemImage.enabled = unlocked;
     }
 
-    public void UpdateDisplay()
+    public void OnClick()
     {
-        bool unlocked = CollectibleManager.Instance
-            .IsCollected(collectibleID);
+        Debug.Log("按钮被点击了！");
+        Debug.Log("isUnlocked: " + isUnlocked);
 
-        SetDisplay(unlocked);
+        if (!isUnlocked)
+            return;
+
+        if (descriptionPanel != null && descriptionTextUI != null)
+        {
+            descriptionPanel.SetActive(true);
+            descriptionTextUI.text = description;
+        }
+    }
+
+    public void CloseDescription()
+    {
+        if (descriptionPanel != null)
+            descriptionPanel.SetActive(false);
     }
 }
